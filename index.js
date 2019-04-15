4fec7139ee418ebc6a51c26db7004682cef3a037
Miguel Angel Torres FP <miguelangeltorresfp@gmail.com>
2019-04-16 01:05:08 +0200

    Add dotenv

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
+import start from './src/server';
 
-import start from './src/server'
+dotenv.config();
+console.log('process.env.MONGO_URL: ', process.env.MONGO_URL);
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
 start().listen(port, () => {
-  console.log(`TODO list backend listening on port ${port}!`)
-})
+  console.log(`TODO list backend listening on port ${port}!`);
+});
--- src/server.js
+++ src/server.js
@@ -1,31 +1,31 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import morgan from 'morgan'
-
-import mongoose from 'mongoose'
-import './models/Todo'
-
-import todos from './routes/todos'
+import bodyParser from 'body-parser';
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import './models/Todo';
+import todos from './routes/todos';
 
 const start = () => {
   if (!process.env.MONGO_URL) {
     throw new Error('No mongo url configured, set MONGO_URL env var');
   }
-  mongoose.connect(process.env.MONGO_URL)
+  mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true
+  });
 
-  const app = express()
-  app.use(morgan('combined'))
-  app.use(bodyParser.urlencoded({ extended: false }))
-  app.use(bodyParser.json())
+  const app = express();
+  app.use(morgan('combined'));
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
   //
-  app.use(todos)
+  app.use(todos);
 
   app.use((err, req, res) => {
-    const { message, stack } = err
-    res.status(500).send({ status: 'error', message, stack })
-  })
-  return app
-}
+    const { message, stack } = err;
+    res.status(500).send({ status: 'error', message, stack });
+  });
+  return app;
+};
 
-export default start
+export default start;
